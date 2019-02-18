c6b2314763370c89b51db3430b038fc636751d80
feyzaakyurek <42723218+feyzaakyurek@users.noreply.github.com>
2019-02-18 10:15:54 -0500

    query.py simplified

--- query.py
+++ query.py
@@ -8,9 +8,40 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 import sys
 
+def read_authors(feed, al):
+    # author_list = []
+    # author_set = set()
+    for entry in feed.entries:
+        for name in (author.name for author in entry.authors):
+            # maybe consider case insensitive comparing
+            # if name not in author_set:
+            #     author_list.append((len(author_list),name))
+            #     author_set.add(name)
+            if name not in map(lambda t: t[1], al):
+                al.append((len(al),name))
+    return al
 
 
-def query_arxiv(search_query, start, max_results = -1):
+def read_collabs(feed, author_list, cl):
+    collab_list = []
+    for entry in feed.entries:
+        title = entry.title
+        arxiv_id = entry.id.split('/abs/')[-1]
+        authors = (author.name for author in entry.authors)
+        for a1,a2 in combinations(authors,2):
+            collab_list.append((find_id(a1,author_list),find_id(a2,author_list),arxiv_id,title))
+    return cl + collab_list
+
+
+def find_id(a, author_list):
+    return [y[1] for y in author_list].index(a)
+
+
+def create_df(l, schema):
+    rdd = sc.parallelize(l)
+    return spark.createDataFrame(rdd,schema)
+
+def send_query(search_query, start, max_results = -1):
     base_url = 'http://export.arxiv.org/api/query?'
     query = 'search_query=%s&start=%i%s' % (search_query,
                                           start,
@@ -23,40 +54,10 @@ def query_arxiv(search_query, start, max_results = -1):
 
     return feed
 
-
-def read_authors(feed):
-    author_list = []
-    for entry in feed.entries:
-        for name in (author.name for author in entry.authors):
-            # maybe consider case insensitive comparing
-            if name not in map(lambda t: t[1], author_list):
-                author_list.append((len(author_list),name))
-    return author_list
-
-
-def read_collabs(feed, author_list):
-    collab_list = []
-    for entry in feed.entries:
-        title = entry.title
-        arxiv_id = entry.id.split('/abs/')[-1]
-        authors = (author.name for author in entry.authors)
-        for a1,a2 in combinations(authors,2):
-            collab_list.append((find_id(a1,author_list),find_id(a2,author_list),arxiv_id,title))
-    return collab_list
-
-
-def find_id(a, author_list):
-    return [y[1] for y in author_list].index(a)
-
-
-def create_df(l, schema):
-    rdd = sc.parallelize(l)
-    return spark.createDataFrame(rdd,schema)
-
-def query(search_query = "all", start = 0, max_results = 2000, verbose = False):
+def query(search_query, al, cl, start = 0, max_results = 2000, verbose = False):
     ### Querying Arxiv API
 
-    feed = query_arxiv(search_query, start, max_results)
+    feed = send_query(search_query, start, max_results)
     print("Search q %s, start: %i, max_results: %i" % (search_query, start, max_results))
     count = len(feed.entries)
     if verbose:
@@ -66,33 +67,62 @@ def query(search_query = "all", start = 0, max_results = 2000, verbose = False):
 
 
     # Create a data frame for unique authors <AuthorID, AuthorName>
-    author_list = read_authors(feed)
-    collab_list = read_collabs(feed, author_list)
+    author_list = read_authors(feed, al)
+    collab_list = read_collabs(feed, author_list, cl)
 
-    return (author_list, collab_list, count, feed.feed.opensearch_totalresults)
+    return (author_list, collab_list, count, int(feed.feed.opensearch_totalresults))
 
 
 def query_all(search_query = "all", request = -1, batch = 1000):
-    al, cl, t, feedtotal = query(search_query, start = 0, max_results = batch, verbose = True)
-    totalresults = t
-    print("Query returned: ", t)
-    print("Total Results: ", totalresults)
-    req = feedtotal if request == -1 else request
+    """ Query Arxiv and store parquet files
 
-    while t == batch and totalresults < req:
-        a, c, t, _ = query(search_query, start = totalresults, max_results = batch, verbose = False)
-        al = al + a
-        cl = cl + c
+    This method takes in the search_query string, maximum
+    number of requests and the size of each query to arxiv.
+    The maximum batch size (query size) cannot be larger
+    than 2000 according to Arxiv API.
+
+    Keyword Arguments:
+    search_query -- query string
+    request -- requested number of papers
+    batch -- size of each single query to Arxiv API.
+    """
+
+    # send to Arxiv to learn total number of papers complying the filter
+    trial_feed = send_query(search_query, 0, 1)
+    feedtotal = int(trial_feed.feed.opensearch_totalresults)
+    print('Total results for this query: %i' % feedtotal)
+
+    # create lists which will be converted to parquet files
+    al = []
+    cl = []
+
+    # if request is not provided, set req to feedtotal
+    req = feedtotal if request == -1 else min(request,feedtotal)
+    t = batch
+    totalresults = 0
+
+    # send queries until req is fulfilled
+    while totalresults < req: #(t == batch and totalresults < req) or (request == -1 and totalresults < feedtotal):
+        feed = send_query(search_query, totalresults, batch)
+        al = read_authors(feed, al)
+        cl = read_collabs(feed, author_list, cl)
+        t = len(feed.entries)
+        # al, cl, t, _ = query(search_query, al, cl, start = totalresults, max_results = batch, verbose = False)
+        # al = al + a
+        # cl = cl + c
         totalresults = totalresults + t
         print("Query returned: ", t)
         print("Total results:%i" % totalresults)
+        # if t == 0: t = batch #potential bug
 
+    # create a dataframe containing author ids and names
     schema_a = StructType([
             StructField("id", IntegerType(), True),
             StructField("name", StringType(), True)
         ])
     author_df = create_df(al, schema_a)
 
+    # create a dataframe containing collaborations
     schema_c = StructType([
             StructField("src", IntegerType(), True),
             StructField("dest", IntegerType(), True),
@@ -101,12 +131,19 @@ def query_all(search_query = "all", request = -1, batch = 1000):
         ])
     collab_df = create_df(cl, schema_c)
 
+    # print results to parquet
     print("Query completed, length of unique authors: ", author_df.count() )
     print("Length of collabs: ", collab_df.count() )
-    author_df.write.mode('overwrite').parquet("Data/authors-%s-total%i.parquet" % (search_query.replace(":",""), totalresults))
-    collab_df.write.mode('overwrite').parquet("Data/collab-%s-total%i.parquet" % (search_query.replace(":",""), totalresults))
+    author_df.write.mode('overwrite') \
+            .parquet("Data/authors-%s-total%i.parquet" % (search_query.replace(":",""), totalresults))
+    collab_df.write.mode('overwrite') \
+            .parquet("Data/collab-%s-total%i.parquet" % (search_query.replace(":",""), totalresults))
     print("Parquet written.")
 
+
+
+
+# main method
 spark = SparkSession.builder \
         .master("local") \
         .appName("Word Count") \
